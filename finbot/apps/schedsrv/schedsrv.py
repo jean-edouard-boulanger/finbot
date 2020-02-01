d745ec5dd3d1af2ce84f9a7a738a03cb0094b7fb
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2020-02-01 12:04:11 +0000

    schedsrv runs workflow for all users

--- finbot/apps/schedsrv/schedsrv.py
+++ finbot/apps/schedsrv/schedsrv.py
@@ -1,8 +1,12 @@
 from finbot.clients.snap import SnapClient
 from finbot.clients.history import HistoryClient
+from finbot.core import dbutils
+from finbot.model import UserAccount
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from functools import partial
-import schedule
 import time
+import traceback
 import logging.config
 import logging
 import os
@@ -31,6 +35,8 @@ def run_workflow(user_account_id, snap_client, hist_client):
     logging.info(f"will take raw snapshot")
 
     snapshot_metadata = snap_client.take_snapshot(user_account_id)
+    logging.debug(snapshot_metadata)
+
     snapshot_id = snapshot_metadata["snapshot"]["identifier"]
     
     logging.info(f"raw snapshot created with id={snapshot_id}")
@@ -44,10 +50,13 @@ def run_workflow(user_account_id, snap_client, hist_client):
     logging.info(f"history report written with id={history_entry_id}")
     logging.debug(history_metadata)
 
-    logging.info("workflow done")
+    logging.info(f"workflow done for user_id={user_account_id}")
 
 
 def main():
+    db_engine = create_engine(os.environ['FINBOT_DB_URL'])
+    db_session = dbutils.add_persist_utilities(scoped_session(sessionmaker(bind=db_engine)))
+
     snapwsrv_endpoint = os.environ["FINBOT_SNAPWSRV_ENDPOINT"]
     snap_client = SnapClient(snapwsrv_endpoint)
     logging.info(f"snapshot client created with {snapwsrv_endpoint} endpoint")
@@ -56,16 +65,17 @@ def main():
     hist_client = HistoryClient(histwsrv_endpoint)
     logging.info(f"history report client created with {histwsrv_endpoint} endpoint")
 
-    schedule.every(1).hour.do(lambda: run_workflow(
-        user_account_id=os.environ["FINBOT_SELECTED_USER"],
-        snap_client=snap_client, 
-        hist_client=hist_client))
-
-
-    schedule.run_all()
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        for user_account in db_session.query(UserAccount).all():
+            try:
+                run_workflow(
+                    user_account_id=user_account.id,
+                    snap_client=snap_client, 
+                    hist_client=hist_client)
+            except Exception as e:
+                logging.warn(f"failure while running workflow for "
+                              "user_id={user_account.id}: {e}, trace: \n{traceback.format_exc()}")
+        time.sleep(3600)
 
 
 if __name__ == "__main__":
--- schedsrv.Dockerfile
+++ schedsrv.Dockerfile
@@ -16,5 +16,8 @@ ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait
 RUN chmod +x /usr/bin/wait
 
 RUN pip3 install \
-    schedule \
-    requests
+    requests \
+    psycopg2 \
+    sqlalchemy \
+    pytz
+
