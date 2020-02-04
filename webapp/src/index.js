d59c702c819813902f8e1ddcfc1867ee5b8fda86
Alina Kraft <mirgehendieideenaus@gmail.com>
2020-02-04 14:27:19 +0100

    move components, add routing, add hooks to app.js to handle user state

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,28 +1,14 @@
-import 'bootswatch/dist/lux/bootstrap.min.css';
-import "./assets/css/index.css"
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
-import 'datejs'
 
-//core components
-import App from './App';
+import App from "./App"
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
     <Router history={hist}>
-        <Switch>
-            <Route exact path="/" component={App} />
-            {/* <Route component={Error}/> */}
-        </Switch>
-    </Router>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
+        <App />
+    </Router>,
+    document.getElementById('root'));
