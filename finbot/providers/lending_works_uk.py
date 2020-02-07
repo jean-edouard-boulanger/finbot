f37ba99ace247f2fc23272e47e57efc666a9caa6
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2020-02-07 22:08:37 +0000

    Rewrite lending works

--- finbot/providers/lending_works_uk.py
+++ finbot/providers/lending_works_uk.py
@@ -1,12 +1,7 @@
-from price_parser import Price
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-from selenium.common.exceptions import StaleElementReferenceException
 from finbot import providers
 from finbot.providers.support.selenium import any_of, get_cookies
-from finbot.providers.errors import AuthFailure
-from copy import deepcopy
+from finbot.providers.errors import AuthFailure, Error
 import requests
 import logging
 import csv
@@ -14,13 +9,9 @@ import io
 
 
 AUTH_URL = "https://www.lendingworks.co.uk/sign-in"
-CLASSIC_URL = "https://www.lendingworks.co.uk/lending-centre/classic"
-LOANS_EXPORT_URL = "https://www.lendingworks.co.uk/lending-centre/classic/my-loans/export"
-
-
-def json_dumps(data):
-    import json
-    return json.dumps(data, indent=4)
+BASE_URL = "https://www.lendingworks.co.uk/lending-centre"
+DASHBOARD_URL = "https://www.lendingworks.co.uk/lending-centre/{ns}"
+LOANS_EXPORT_URL = "https://www.lendingworks.co.uk/lending-centre/{ns}/my-loans/export"
 
 
 def is_logged_in(browser):
@@ -33,20 +24,6 @@ def has_error(browser):
 
 def get_error(browser):
     return browser.find_element_by_css_selector("div.alert-block").text.strip()
-
-
-def balances_loaded(browser):
-    wallet_block = browser.find_element_by_css_selector("div.lw-card-wallet")
-    return len(wallet_block.find_element_by_css_selector("div.lw-amount")
-                           .text.strip()) > 0
-
-
-def validate_accounts(account_ids=None):
-    if account_ids is None:
-        return
-    for account_id in account_ids:
-        if account_id not in {"wallet", "offers", "loan"}:
-            raise RuntimeError(f"unknown account {account_id}")
 
 
 class Credentials(object):
@@ -66,15 +43,66 @@ class Credentials(object):
 class Api(providers.SeleniumBased):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._accounts = {}
 
-    def _is_home(self):
-        return self.browser.current_url == CLASSIC_URL
+    def _get_loans(self, account_type):
+        def _chunk_outstanding(row):
+            return float(row["Chunk Amount"]) - float(row["Chunk Capital Repaid"])
+        cookies = get_cookies(self.browser)
+        loans_endpoint = LOANS_EXPORT_URL.format(ns=account_type)
+        response = requests.get(loans_endpoint, cookies=cookies)
+        csv_data = response.content.decode()
+        all_data = list(csv.DictReader(io.StringIO(csv_data)))
+        return [
+            {
+                "name": f"{row['Loan ID']}-{row['Chunk ID']}",
+                "type": "loan",
+                "value": _chunk_outstanding(row),
+            }
+            for row in all_data
+        ]
 
-    def _go_home(self):
-        if not self._is_home():
-            self.browser.get(CLASSIC_URL)
-        side_bar = self._do.wait_element(By.CSS_SELECTOR, "div.m-lw-db-region-sidebar")
-        self._do.wait_cond(balances_loaded)
+    def _get_account_data(self, account_key):
+        def do_get_account_data(_):
+            all_data = self.browser.execute_script("return window.data.appData;")
+            account_data = all_data[account_type]
+            if "total_account_balance" in account_data:
+                return account_data
+            return None
+        account_type, _ = account_key
+        if not self._accounts[account_key]:
+            account_url = DASHBOARD_URL.format(ns=account_type)
+            self._do.get(account_url)
+            self._accounts[account_key] = {
+                "summary": self._do.wait_cond(do_get_account_data),
+                "loans": self._get_loans(account_type)
+            }
+        return self._accounts[account_key]
+
+    def _iter_accounts(self):
+        for account_key in self._accounts:
+            account_type, account_name = account_key
+            data = self._get_account_data(account_key)
+            yield {
+                "account": {
+                    "id": account_type,
+                    "name": account_name,
+                    "iso_currency": "GBP"
+                },
+                "balance": data["summary"]["total_account_balance"],
+                "assets": [
+                    {
+                        "name": "offers", 
+                        "type": "currency", 
+                        "value": data["summary"]["total_offers"]
+                    },
+                    {
+                        "name": "wallet",
+                        "type": "currency",
+                        "value": data["summary"]["wallet_total"]
+                    }
+                ] + data["loans"]
+            }
 
     def authenticate(self, credentials):
         browser = self.browser
@@ -88,89 +116,31 @@ class Api(providers.SeleniumBased):
         self._do.wait_cond(any_of(is_logged_in, has_error))
         if not is_logged_in(browser):
             raise AuthFailure(get_error(browser))
+        self._do.wait_element(By.CLASS_NAME, "onboarding-heading")
+        classic_btn, ifisa_btn = self._do.find_many(By.CSS_SELECTOR, "div.btn")
+        if "view" in classic_btn.text.lower():
+            self._accounts[("classic", "Classic account")] = None
+        if "view" in ifisa_btn.text.lower():
+            self._accounts[("ifisa", "ISA account")] = None
 
     def get_balances(self):
-        def extract_balance(account_id, account_block):
-            amount_element = account_block.find_element_by_css_selector("div.lw-amount")
-            amount = Price.fromstring(amount_element.text.strip())
-            return {
-                "account": {
-                    "id": account_id,
-                    "name": account_id.title(),
-                    "iso_currency": "GBP" # TODO,
-                },
-                "balance": amount.amount_float
-            }
-        self._go_home()
         return {
             "accounts": [
-                extract_balance(account_id, block) for (account_id, block) in [
-                    ("wallet", self._do.find(By.CSS_SELECTOR, "div.lw-card-wallet")),
-                    ("offers", self._do.find(By.CSS_SELECTOR, "div.lw-card-queue")),
-                    ("loan", self._do.find(By.CSS_SELECTOR, "div.lw-card-loan"))
-                ]
+                {
+                    "account": entry["account"],
+                    "balance": entry["balance"]
+                }
+                for entry in self._iter_accounts()
             ]
         }
-
-    def _get_wallet_assets(self):
-        self._go_home()
-        browser = self.browser
-        amount_str = (self._do.find(By.CSS_SELECTOR, "div.lw-card-wallet")
-                          .find_element_by_css_selector("div.lw-amount")
-                          .text.strip())
-        return [{
-            "name": "cash",
-            "type": "currency",
-            "value": Price.fromstring(amount_str).amount_float,
-            "provider_specific": None
-        }]
-
-    def _get_offers_assets(self):
-        self._go_home()
-        browser = self.browser
-        amount_str = (self._do.find(By.CSS_SELECTOR, "div.lw-card-queue")
-                          .find_element_by_css_selector("div.lw-amount")
-                          .text.strip())
-        return [{
-            "name": "cash",
-            "type": "currency",
-            "value": Price.fromstring(amount_str).amount_float,
-            "provider_specific": None
-        }]
-
-    def _get_loan_assets(self):
-        def _chunk_outstanding(row):
-            return float(row["Chunk Amount"]) - float(row["Chunk Capital Repaid"])
-        cookies = get_cookies(self.browser)
-        response = requests.get(LOANS_EXPORT_URL, cookies=cookies)
-        csv_data = response.content.decode()
-        all_data = list(csv.DictReader(io.StringIO(csv_data)))
-        total_outstanding = sum(_chunk_outstanding(row) for row in all_data)
-        return [
-            {
-                "name": f"{row['Loan ID']}-{row['Chunk ID']}" ,
-                "type": "loan",
-                "value": _chunk_outstanding(row),
-                "provider_specific": deepcopy(dict(row))
-            }
-            for row in all_data
-        ]
 
     def get_assets(self):
         return {
             "accounts": [
                 {
-                    "account": {
-                        "id": account_id,
-                        "name": account_id.title(),
-                        "iso_currency": "GBP"
-                    },
-                    "assets": handler()
+                    "account": entry["account"],
+                    "assets": entry["assets"]
                 }
-                for account_id, handler in [
-                    ("wallet", self._get_wallet_assets),
-                    ("offers", self._get_offers_assets),
-                    ("loan", self._get_loan_assets)
-                ]
+                for entry in self._iter_accounts()
             ]
         }
