8112cb221f909ba88644efde50ce258f32dc60a2
Alina Kraft <mirgehendieideenaus@gmail.com>
2020-02-09 23:47:53 +0100

    fix navbar

--- webapp/src/Navbar/Navbar.js
+++ webapp/src/Navbar/Navbar.js
@@ -1,17 +1,11 @@
 import React, { useEffect, useContext } from "react";
 import { withRouter } from "react-router";
-<<<<<<< HEAD
 import { NavLink } from 'react-router-dom'
 
-=======
-
-import Navbar from 'react-bootstrap/Navbar';
->>>>>>> master
 import NavBar from 'react-bootstrap/NavBar';
 import Nav from 'react-bootstrap/Nav';
 import ProvidersDropdown from "./Providers";
 import ProvidersContext from "../context/LinkedAccountContext";
-<<<<<<< HEAD
 import AuthContext from "../context/authContext";
 
 const Navigation = props => {
@@ -19,13 +13,6 @@ const Navigation = props => {
     const authContext = useContext(AuthContext);
     const { _selectProvider, _awaitProviders, selectedProvider } = providersContext;
     const { token } = authContext;
-=======
-
-
-const Navigation = props => {
-    const providersContext = useContext(ProvidersContext)
-    const { providersList, _selectProvider, _awaitProviders, selectedProvider } = providersContext;
->>>>>>> master
 
     //component first mount
     useEffect(() => {
@@ -43,7 +30,6 @@ const Navigation = props => {
 
     return (
         <NavBar bg="dark" variant="dark">
-<<<<<<< HEAD
             {/* <Navbar.Brand href="#home">Finbot</Navbar.Brand> */}
             <NavLink className="px-5 navbar-brand" to="/">Finbot</NavLink>
 
@@ -53,29 +39,13 @@ const Navigation = props => {
                         <NavLink className="px-5 nav-link" to="/">Home</NavLink>
                         <NavLink className="px-5 nav-link" to="/auth/logout">Logout</NavLink>
                         <ProvidersDropdown _setProvider={_setProvider} />
-=======
-            <Navbar.Brand href="#home">Finbot</Navbar.Brand>
-
-            {props.user ?
-                <>
-                    <Nav activeKey={props.location.pathname} className="ml-auto">
-                        <Nav.Link className="px-5" href="/">Home</Nav.Link>
-                        <Nav.Link className="px-5" href="/auth/logout">Logout</Nav.Link>
-                        <ProvidersDropdown _setProvider={_setProvider} />
-                        {/* <ProvidersDropdown options={props.providers} selectOption={selectProvider} /> */}
->>>>>>> master
                     </Nav>
                 </>
                 :
                 <>
                     <Nav activeKey={props.location.pathname} className="ml-auto">
-<<<<<<< HEAD
                         <NavLink className="px-5 nav-link" to="/auth/sign-up">Sign Up</NavLink>
                         <NavLink className="px-5 nav-link" to="/auth/logout">Log In</NavLink>
-=======
-                        <Nav.Link href="/auth/sign-up">Sign Up</Nav.Link>
-                        <Nav.Link href="/auth/log-in">Log In</Nav.Link>
->>>>>>> master
                     </Nav>
                 </>
             }
