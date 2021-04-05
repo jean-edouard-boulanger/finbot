dcb356dd81ae15703603e27a5313f0138cb6aeb7
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2021-04-05 12:43:48 +0100

    black

--- finbot/providers/credit_agricole_fr.py
+++ finbot/providers/credit_agricole_fr.py
@@ -131,6 +131,8 @@ def _get_login_error(do: SeleniumHelper):
     ]
     if error_items:
         return error_items[0].text
-    strong_auth_items = do.find_many(By.XPATH, f"//*[contains(text(),'authentification forte')]")
+    strong_auth_items = do.find_many(
+        By.XPATH, f"//*[contains(text(),'authentification forte')]"
+    )
     if strong_auth_items:
         return "strong authentication required"
