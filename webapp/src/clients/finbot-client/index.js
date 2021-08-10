ac9e862bb3429fa21a24bb9da15f85cf293ab463
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2021-08-10 12:35:43 +0200

    Display proper linked account status

--- webapp/src/clients/finbot-client/index.js
+++ webapp/src/clients/finbot-client/index.js
@@ -16,7 +16,8 @@ class FinbotClient {
   handleResponse(response) {
     const app_data = response.data;
     if (Object.prototype.hasOwnProperty.call(app_data, "error")) {
-      throw app_data.error.debug_message;
+      console.log(app_data.error);
+      throw app_data.error.user_message;
     }
     return app_data;
   }
