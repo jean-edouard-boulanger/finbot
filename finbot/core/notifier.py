b096cfe6a9b9a68b704b7ec002732f4f0db25fab
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2023-05-28 17:33:36 +0200

    Notify by email when linked accounts snapshot error (#155)

--- finbot/core/notifier.py
+++ finbot/core/notifier.py
@@ -1,8 +1,11 @@
-from dataclasses import dataclass
 from textwrap import dedent
 from typing import Protocol
 
+from pydantic import BaseModel
 from twilio.rest import Client as TwilioClient
+
+from finbot import model
+from finbot.core import email_delivery
 
 
 class Notifier(Protocol):
@@ -14,19 +17,50 @@ class Notifier(Protocol):
     def notify_twilio_settings_updated(self) -> None:
         ...
 
+    def notify_linked_accounts_snapshot_errors(
+        self, error_entries: list[model.LinkedAccountSnapshotEntry]
+    ) -> None:
+        ...
 
-@dataclass
-class TwilioSettings:
+
+class TwilioSettings(BaseModel):
     account_sid: str
     auth_token: str
     phone_number: str
 
-    @staticmethod
-    def deserialize(data: dict[str, str]) -> "TwilioSettings":
-        return TwilioSettings(
-            account_sid=data["account_sid"],
-            auth_token=data["auth_token"],
-            phone_number=data["phone_number"],
+
+class EmailNotifier(Notifier):
+    def __init__(
+        self,
+        email_delivery_settings: email_delivery.DeliverySettings,
+        recipient_email: str,
+    ):
+        self._email_delivery_settings = email_delivery_settings
+        self._recipient_email = recipient_email
+        self._service = email_delivery.EmailService(email_delivery_settings)
+
+    def notify_valuation(
+        self, valuation: float, change_1day: float | None, currency: str
+    ) -> None:
+        pass
+
+    def notify_twilio_settings_updated(self) -> None:
+        pass
+
+    def notify_linked_accounts_snapshot_errors(
+        self, error_entries: list[model.LinkedAccountSnapshotEntry]
+    ) -> None:
+        self._service.send_email(
+            email=email_delivery.Email(
+                recipients_emails=[self._recipient_email],
+                subject=f"There is an issue with {len(error_entries)} of your linked account(s)",
+                body=dedent(
+                    f"""\
+                Finbot failed to get a snapshot from the following linked accounts: \
+                {(', '.join(entry.linked_account.account_name for entry in error_entries)).strip()}
+                """
+                ),
+            )
         )
 
 
@@ -61,17 +95,28 @@ class TwilioNotifier(Notifier):
             ).strip(),
         )
 
+    def notify_linked_accounts_snapshot_errors(
+        self, error_entries: list[model.LinkedAccountSnapshotEntry]
+    ) -> None:
+        pass
+
 
 class CompositeNotifier(Notifier):
     def __init__(self, notifiers: list[Notifier]):
-        self.notifiers = notifiers
+        self._notifiers = notifiers
 
     def notify_valuation(
         self, valuation: float, change_1day: float | None, currency: str
     ) -> None:
-        for notifier in self.notifiers:
+        for notifier in self._notifiers:
             notifier.notify_valuation(valuation, change_1day, currency)
 
     def notify_twilio_settings_updated(self) -> None:
-        for notifier in self.notifiers:
+        for notifier in self._notifiers:
             notifier.notify_twilio_settings_updated()
+
+    def notify_linked_accounts_snapshot_errors(
+        self, error_entries: list[model.LinkedAccountSnapshotEntry]
+    ) -> None:
+        for notifier in self._notifiers:
+            notifier.notify_linked_accounts_snapshot_errors(error_entries)
