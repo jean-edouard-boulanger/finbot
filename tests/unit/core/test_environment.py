e0111a77997044783f6cee6b67f080b1184e7c60
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2023-06-05 08:17:56 +0200

    add test_get_finbot_runtime

--- tests/unit/core/test_environment.py
+++ tests/unit/core/test_environment.py
@@ -2,23 +2,37 @@ from unittest.mock import patch
 import pytest
 import os
 
-from finbot.core.environment import get_environment_value, MissingEnvironment
+from finbot.core import environment
 
 
 def test_get_environment_value():
     with patch.dict(os.environ, {"SOME_KEY": "SOME_VALUE"}, clear=True):
-        assert get_environment_value("SOME_KEY") == "SOME_VALUE"
+        assert environment.get_environment_value("SOME_KEY") == "SOME_VALUE"
 
 
 def test_get_environment_value_returns_default():
     with patch.dict(os.environ, {}, clear=True):
         assert (
-            get_environment_value("SOME_MISSING_KEY", "SOME_DEFAULT_VALUE")
+            environment.get_environment_value("SOME_MISSING_KEY", "SOME_DEFAULT_VALUE")
             == "SOME_DEFAULT_VALUE"
         )
 
 
 def test_get_environment_value_raises_when_variable_is_missing():
     with patch.dict(os.environ, {}, clear=True):
-        with pytest.raises(MissingEnvironment):
-            get_environment_value("SOME_MISSING_KEY_2")
+        with pytest.raises(environment.MissingEnvironment):
+            environment.get_environment_value("SOME_MISSING_KEY_2")
+
+
+@pytest.mark.parametrize(
+    "environ_mock,expected_runtime",
+    [
+        ({}, environment.PRODUCTION_ENV),
+        ({"FINBOT_ENV": environment.PRODUCTION_ENV}, environment.PRODUCTION_ENV),
+        ({"FINBOT_ENV": "bad_value"}, environment.PRODUCTION_ENV),
+        ({"FINBOT_ENV": environment.DEVELOPMENT_ENV}, environment.DEVELOPMENT_ENV),
+    ],
+)
+def test_get_finbot_runtime(environ_mock: dict[str, str], expected_runtime: str):
+    with patch.dict(os.environ, environ_mock, clear=True):
+        assert environment.get_finbot_runtime() == expected_runtime
