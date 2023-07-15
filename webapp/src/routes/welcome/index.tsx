9b12f9fb69832d10be30eb36f33b88f679eca12b
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2023-07-15 09:31:13 +0200

    Revert "Change the basename to /finbot/ (#210)" (#211)

--- webapp/src/routes/welcome/index.tsx
+++ webapp/src/routes/welcome/index.tsx
@@ -22,10 +22,7 @@ export const Welcome: React.FC<WelcomeProps> = () => {
       </Row>
       <Row>
         <Col md={2} className={"mr-4"}>
-          <img
-            src={`${process.env.PUBLIC_URL}/finbot_medium.png`}
-            alt={"finbot"}
-          />
+          <img src={"/finbot_medium.png"} alt={"finbot"} />
         </Col>
         <Col md={6}>
           <Card>
