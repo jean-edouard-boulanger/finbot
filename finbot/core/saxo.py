aeea2658a3a921541caf6dcf4e2cec8d8ea3db5d
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2023-10-03 14:24:59 +0200

    Add ETF support for Saxo provider

--- finbot/core/saxo.py
+++ finbot/core/saxo.py
@@ -2,6 +2,64 @@ from dataclasses import dataclass
 from typing import Any, Literal
 
 import requests
+from pydantic import BaseModel
+
+
+class DisplayAndFormat(BaseModel):
+    Currency: str
+    Description: str
+    Symbol: str
+
+
+class Exchange(BaseModel):
+    Description: str
+    ExchangeId: str
+
+
+class SinglePositionBase(BaseModel):
+    AccountId: str
+    AccountKey: str
+    Amount: float
+    AssetType: str
+
+
+class SinglePositionView(BaseModel):
+    CurrentPrice: float
+    MarketValue: float
+    ProfitLossOnTrade: float
+
+
+class SinglePosition(BaseModel):
+    PositionBase: SinglePositionBase
+    PositionId: str
+    PositionView: SinglePositionView
+
+
+class NetPosition(BaseModel):
+    DisplayAndFormat: DisplayAndFormat
+    Exchange: Exchange
+    NetPositionId: str
+    SinglePosition: SinglePosition
+
+
+class NetPositionsResponse(BaseModel):
+    Data: list[NetPosition]
+
+
+class Account(BaseModel):
+    AccountKey: str
+    ClientKey: str
+    Currency: str
+    DisplayName: str
+
+
+class AccountsResponse(BaseModel):
+    Data: list[Account]
+
+
+class AccountBalancesResponse(BaseModel):
+    CashAvailableForTrading: float
+    TotalValue: float
 
 
 @dataclass(frozen=True)
@@ -26,15 +84,60 @@ class SaxoGatewayClient:
         except Exception:
             return False
 
-    def send_openapi_request(self, verb: Literal["GET"], resource: str) -> Any:
+    def send_openapi_request(
+        self,
+        verb: Literal["GET"],
+        resource: str,
+        params: dict[str, str] | None = None,
+    ) -> Any:
         resource = resource.lstrip("/")
         impl = getattr(self._session, verb.lower())
         response = impl(
             f"{self._settings.gateway_url}/openapi/{resource}",
             headers={"Authorization": f"Bearer {self._api_key}"},
+            params=params,
         )
         response.raise_for_status()
         return response.json()
 
-    def openapi_get(self, resource: str) -> Any:
-        return self.send_openapi_request("GET", resource)
+    def openapi_get(
+        self,
+        resource: str,
+        params: dict[str, str] | None = None,
+    ) -> Any:
+        return self.send_openapi_request(
+            verb="GET",
+            resource=resource,
+            params=params,
+        )
+
+    def get_accounts(self) -> AccountsResponse:
+        accounts_response_payload = self.openapi_get("port/v1/accounts/me")
+        return AccountsResponse.parse_obj(accounts_response_payload)
+
+    def get_account_positions(
+        self,
+        saxo_account: Account,
+    ) -> NetPositionsResponse:
+        net_positions_payload = self.openapi_get(
+            "port/v1/netpositions/",
+            params={
+                "AccountKey": saxo_account.AccountKey,
+                "ClientKey": saxo_account.ClientKey,
+                "FieldGroups": "SinglePositionBase,SinglePositionView,DisplayAndFormat,ExchangeInfo",
+            },
+        )
+        return NetPositionsResponse.parse_obj(net_positions_payload)
+
+    def get_account_balances(
+        self,
+        saxo_account: Account,
+    ) -> AccountBalancesResponse:
+        account_balances_response_payload = self.openapi_get(
+            "port/v1/balances/",
+            params={
+                "AccountKey": saxo_account.AccountKey,
+                "ClientKey": saxo_account.ClientKey,
+            },
+        )
+        return AccountBalancesResponse.parse_obj(account_balances_response_payload)
--- finbot/providers/saxo_gw_fr.py
+++ finbot/providers/saxo_gw_fr.py
@@ -1,9 +1,9 @@
-from typing import Any, Generator, cast
+from typing import Any, Generator
 
 from pydantic import BaseModel, SecretStr
 
+from finbot.core import saxo
 from finbot.core.environment import get_saxo_gateway_url
-from finbot.core.saxo import SaxoGatewayClient, SaxoGatewaySettings
 from finbot.providers.base import ProviderBase
 from finbot.providers.errors import AuthenticationFailure
 from finbot.providers.schema import (
@@ -25,12 +25,12 @@ class Api(ProviderBase):
         super().__init__(**kwargs)
         self._credentials = credentials
         saxo_gateway_url = get_saxo_gateway_url()
-        assert saxo_gateway_url is not None
-        self._client = SaxoGatewayClient(
-            settings=SaxoGatewaySettings(gateway_url=saxo_gateway_url),
+        assert saxo_gateway_url, "Saxo provider is not properly configured"
+        self._client = saxo.SaxoGatewayClient(
+            settings=saxo.SaxoGatewaySettings(gateway_url=saxo_gateway_url),
             api_key=credentials.api_key.get_secret_value(),
         )
-        self._accounts: list[Any] | None = None
+        self._accounts: list[saxo.Account] | None = None
 
     @staticmethod
     def description() -> str:
@@ -42,17 +42,17 @@ class Api(ProviderBase):
 
     def initialize(self) -> None:
         try:
-            self._accounts = self._client.openapi_get("port/v1/accounts/me")["Data"]
+            self._accounts = self._client.get_accounts().Data
         except Exception as e:
             raise AuthenticationFailure(str(e))
 
-    def iter_accounts(self) -> Generator[tuple[Account, Any], None, None]:
+    def iter_accounts(self) -> Generator[tuple[Account, saxo.Account], None, None]:
         assert self._accounts is not None
         for raw_account_data in self._accounts:
             yield Account(
-                id=raw_account_data["AccountKey"],
-                name=raw_account_data["AccountGroupName"],
-                iso_currency=raw_account_data["Currency"],
+                id=raw_account_data.AccountKey,
+                name=raw_account_data.DisplayName,
+                iso_currency=raw_account_data.Currency,
                 type="investment",
             ), raw_account_data
 
@@ -61,12 +61,11 @@ class Api(ProviderBase):
             accounts=[
                 BalanceEntry(
                     account=account,
-                    balance=self._get_account_balance(
-                        account_key=raw_account_data["AccountKey"],
-                        client_key=raw_account_data["ClientKey"],
-                    ),
+                    balance=self._client.get_account_balances(
+                        saxo_account=saxo_account
+                    ).TotalValue,
                 )
-                for (account, raw_account_data) in self.iter_accounts()
+                for (account, saxo_account) in self.iter_accounts()
             ]
         )
 
@@ -75,26 +74,44 @@ class Api(ProviderBase):
             accounts=[
                 AssetsEntry(
                     account=account,
-                    assets=self._get_account_assets(
-                        account_key=raw_account_data["AccountKey"],
-                        client_key=raw_account_data["ClientKey"],
-                    ),
+                    assets=self._get_account_assets(saxo_account=saxo_account),
                 )
-                for (account, raw_account_data) in self.iter_accounts()
+                for (account, saxo_account) in self.iter_accounts()
             ]
         )
 
-    def _get_account_assets(self, account_key: str, client_key: str) -> list[Asset]:
+    def _get_account_assets(self, saxo_account: saxo.Account) -> list[Asset]:
         assets: list[Asset] = []
-        balance_data = self._client.openapi_get(
-            f"port/v1/balances?AccountKey={account_key}&ClientKey={client_key}"
-        )
-        if cash_available := balance_data.get("CashAvailableForTrading"):
-            assets.append(Asset(name="Cash", type="currency", value=cash_available))
+        if cash_available := self._client.get_account_balances(
+            saxo_account
+        ).CashAvailableForTrading:
+            assets.append(
+                Asset(
+                    name=f"Cash ({saxo_account.Currency})",
+                    type="currency",
+                    value=cash_available,
+                )
+            )
+        for position in self._client.get_account_positions(saxo_account).Data:
+            assets.append(_make_asset(position))
         return assets
 
-    def _get_account_balance(self, account_key: str, client_key: str) -> float:
-        balance_data = self._client.openapi_get(
-            f"port/v1/balances?AccountKey={account_key}&ClientKey={client_key}"
+
+def _make_asset(positions: saxo.NetPosition) -> Asset:
+    asset_type = positions.SinglePosition.PositionBase.AssetType
+    if asset_type.lower() in ("etf",):
+        return Asset(
+            name=positions.DisplayAndFormat.Description,
+            type="equity",
+            value=positions.SinglePosition.PositionView.MarketValue,
+            units=positions.SinglePosition.PositionBase.Amount,
+            provider_specific={
+                "Symbol": positions.DisplayAndFormat.Symbol,
+                "Description": positions.DisplayAndFormat.Description,
+                "Equity type": positions.SinglePosition.PositionBase.AssetType,
+                "Listing exchange": positions.Exchange.Description,
+                "Current price": positions.SinglePosition.PositionView.CurrentPrice,
+                "P&L": positions.SinglePosition.PositionView.ProfitLossOnTrade,
+            },
         )
-        return cast(float, balance_data["TotalValue"])
+    raise ValueError(f"unknown asset type: {asset_type}")
