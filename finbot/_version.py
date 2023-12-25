ed87df8e0b947fd32c25eaf45313cd8695ad92d6
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2023-12-25 19:42:02 +0100

    bump version to 0.8.9

--- finbot/_version.py
+++ finbot/_version.py
@@ -1,5 +1,5 @@
 # Controls the overall finbot application version. Bump to release a new finbot version.
-__version__ = "0.8.8"
+__version__ = "0.8.9"
 
 # Controls the finbot application API version. Bump whenever the finbot internal schema is updated.
 __api_version__ = "0.0.2"
