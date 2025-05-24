ced008708f9faefceb42a555d630d519b047246d
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2025-05-24 19:59:47 +0200

    Install gnupg (#482)

--- finbot/_version.py
+++ finbot/_version.py
@@ -1,5 +1,5 @@
 # Controls the overall finbot application version. Bump to release a new finbot version.
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 # Controls the finbot application API version. Bump whenever the finbot internal schema is updated.
 __api_version__ = "0.0.6"
